220cadc6046ea499654b399541a134f3d3ecd9a1
Jorge Rodríguez Pedrianes <jrodped@kuflow.com>
2024-09-16 13:01:51 +0100

    Added scripts to manage monorepo

---
+++ scripts/projects.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# all python packages, in topological order
+# Example: PROJECTS='package-a package-b service-c'
+export PROJECTS='
+  kuflow-samples-kubot-desktop-screenshot
+  kuflow-samples-kubot-google-images
+  kuflow-samples-temporal-loan
+'
